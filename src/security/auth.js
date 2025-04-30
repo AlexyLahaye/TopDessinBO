0e22a827292cc4451795a0346a53d96cad6379cb
Alexy <lahaye.alexy1804@gmail.com>
2025-04-30 13:47:08 +0200

    TD-4 modification utilisateur BO + verification du Token en BO
    création d'une méthode en Back pour vérifier la validité du token, exemple effectué sur la route "protégée" modifier user créer partiellement pour l'exemple.

--- src/security/auth.js
+++ src/security/auth.js
@@ -1,5 +1,24 @@
-const { sign } = require('jsonwebtoken');
+const { sign, jwt  } = require('jsonwebtoken');
 
 exports.generateAuthToken = (id, pseudo, droit) => {
     return sign({ id, pseudo, droit}, "zazouestleplusbeau", { expiresIn: 15000 });
+};
+
+exports.verifyToken = (req, res, next) => {
+    const SECRET_KEY = process.env.JWT_SECRET;
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Format : "Bearer token"
+
+    if (!token) {
+        res.status(401).json({ error: "Token manquant" });
+    } else {
+        jwt.verify(token, SECRET_KEY, (err, user) => {
+            if (err) {
+                res.status(403).json({ error: "Token invalide ou expiré" });
+            } else {
+                req.user = user; // Ajoute les infos du token à la requête (id, droit, etc.)
+                next();
+            }
+        });
+    }
 };
