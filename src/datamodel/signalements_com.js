dab8626f4a09095351980626e07aab09197c7f06
Malauryyt <110236103+Malauryyt@users.noreply.github.com>
2025-06-17 16:04:23 +0200

    finalisation de la bd

--- src/datamodel/signalements_com.js
+++ src/datamodel/signalements_com.js
@@ -1,21 +1,12 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('./db');
 
-const reclamationModel = sequelize.define(
-    'reclamations',
+const SignalementsModel = sequelize.define(
+    'signalements_com',
     {
         id: { primaryKey: true, autoIncrement: true , type: DataTypes.INTEGER},
-        type: { type: DataTypes.INTEGER, allowNull: true },
-        commentaire: { type: DataTypes.TEXT, allowNull: true },
-        signalementId: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'signalements_post',
-                key: 'id',
-            },
-            onDelete: 'CASCADE', // facultatif, mais utile pour gérer les suppressions
-            allowNull: false,
-        },
+        description: { type: DataTypes.TEXT, allowNull: true },
+        statut: { type: DataTypes.STRING, allowNull: true },
         userId: {
             type: DataTypes.INTEGER,
             references: {
@@ -24,10 +15,26 @@ const reclamationModel = sequelize.define(
             },
             onDelete: 'CASCADE', // facultatif, mais utile pour gérer les suppressions
             allowNull: false,
-        }
+        },
+        comId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'commentaires',
+                key: 'id',
+            },
+            onDelete: 'CASCADE', // facultatif, mais utile pour gérer les suppressions
+            allowNull: false,
+        },
+        raisonId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'raisons',
+                key: 'id',
+            },
+            allowNull: false,
+        },
 
     },
-    { tableName: 'reclamations' },
+    { tableName: 'signalements_com' },
 );
-
-module.exports = reclamationModel;
+module.exports = SignalementsModel;
